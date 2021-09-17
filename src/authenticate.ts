d6db83e099e0e4fd1eb2f4d8ab14e8c40fde7fdb
Elliana May <me@mause.me>
2021-09-17 05:56:54 +0000

    CORS

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -1,4 +1,12 @@
 import "reflect-metadata";
 import { factory } from "vercel-jwt-auth";
+import { VercelApiHandler, VercelRequest, VercelResponse } from "@vercel/node";
 
-export default factory(process.env.JWT_SECRET!);
+export default (handler: VercelApiHandler) =>
+  async (req: VercelRequest, res: VercelResponse) => {
+    const authenticate = factory(process.env.JWT_SECRET!)(handler);
+
+    if (req.method === "OPTIONS") return res.status(200);
+
+    return await authenticate(req, res);
+  };
