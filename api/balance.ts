824d444a71a4e7c81121fc2a2f45baac5d472cb5
Elliana May <me@mause.me>
2024-11-09 15:03:23 +0800

    Update balance.ts

--- api/balance.ts
+++ api/balance.ts
@@ -3,8 +3,14 @@ import {
   ListTransactionsResponse,
   TransactionsApi,
 } from "../src/up";
+import { IsNumber, IsString } from 'class-validator';
 
-class Shape {}
+class Shape {
+    @IsNumber()
+  valueInBaseUnits!: number;
+  @IsString()
+  value!: string;
+}
 
 export const responseShape = Shape.name;
 
