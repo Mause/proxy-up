a50c694365696d0231e59fb9fd465942382d517a
Elliana May <me@mause.me>
2024-11-09 15:27:31 +0800

    actually return data

--- api/balance.ts
+++ api/balance.ts
@@ -1,12 +1,15 @@
-import {
-  Configuration,
-  ListTransactionsResponse,
-  TransactionsApi,
-} from "../src/up";
-import { IsNumber, IsString } from 'class-validator';
+import { Configuration, AccountsApi } from "../src/up";
+import { IsNumber, IsString } from "class-validator";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+const config = new Configuration({
+  accessToken: process.env.UP_API_KEY,
+});
+
+const accountsApi = new AccountsApi(config);
 
 class Shape {
-    @IsNumber()
+  @IsNumber()
   valueInBaseUnits!: number;
   @IsString()
   value!: string;
@@ -14,6 +17,11 @@ class Shape {
 
 export const responseShape = Shape.name;
 
-export default function () {
-  return 'get the balance right';
+export default async function (req: VercelRequest, res: VercelResponse) {
+  const accounts = await accountsApi.accountsGet();
+  res.json(
+    accounts.data.data.map((account) => {
+      return [account.attributes.displayName, account.attributes.balance];
+    })
+  );
 }
