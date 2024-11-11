8f394301cdfdc64a681d1d99597d624452ba2474
Elliana May <me@mause.me>
2024-11-11 11:31:16 +0800

    better shape

--- api/balance.ts
+++ api/balance.ts
@@ -1,6 +1,7 @@
 import { Configuration, AccountsApi } from "../src/up";
 import { IsNumber, IsString } from "class-validator";
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { Type } from "class-transformer";
 
 const accountsApi = new AccountsApi(
   new Configuration({
@@ -9,6 +10,15 @@ const accountsApi = new AccountsApi(
 );
 
 class Shape {
+  @IsString()
+  id!: string;
+  @IsString()
+  displayName!: string;
+  @Type(() => Money)
+  balance!: Money;
+}
+
+class Money {
   @IsNumber()
   valueInBaseUnits!: number;
   @IsString()
@@ -20,8 +30,12 @@ export const responseShape = Shape.name;
 export default async function (req: VercelRequest, res: VercelResponse) {
   const accounts = await accountsApi.accountsGet();
   res.json(
-    accounts.data.data.map((account) => {
-      return [account.attributes.displayName, account.attributes.balance];
+    accounts.data.data.map(({ attributes }): Shape => {
+      return {
+        id: attributes.id,
+        displayName: attributes.displayName,
+        balance: attributes.balance,
+      };
     })
   );
 }
